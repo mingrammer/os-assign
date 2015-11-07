201a1b05a485cc3787d5656642a4fd044853601a
mathdd <k239507@gmail.com>
2015-11-07 06:08:26 -0800

    merge two different ta prgrams

--- simulator.c
+++ simulator.c
@@ -1,186 +1,148 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <semaphore.h>
+#include <string.h> // memset
+#include <pthread.h> // pthread_t, pthread_create, pthread_join
+#include <semaphore.h> // sem_init, sem_wait, sem_post
+#include <time.h>
 
-#define CHAIR_NUM 3
+#define NUM_SEAT 3
 
-/*
-** Maximum of sleeping time of threads
-*/
 #define SLEEP_MAX 5
 
+//sematphores
+sem_t sem_stu;
+sem_t sem_ta;
+
+//sem_t mutex;
+pthread_mutex_t mutex;
+
+int chair[3];
+int count = 0; //number of waiting students
+int next_seat = 0;
+int next_teach = 0;
+
 void rand_sleep(void);
-
-/*
-** @var threads: Array containing student threads
-** @var queue: Array containing ready tasks
-** @var thread_num: Number of threads
-** @var queue_size: Size of queue
-** @var head: Index of the first element
-** @var tail: Index of the last element
-** @var count: Number of readying tasks
-** @var has_assistant: Check assistant is there
-*/
-typedef struct {
-	pthread_mutex_t lock;
-	pthread_t *threads;
-	long unsigned *queue;
-	int thread_num;
-	int queue_size;
-	int head;
-	int tail;
-	int count;
-	int has_assistant;
-} threadpool_t;
-
-threadpool_t *threadpool_create(int thread_num, int queue_size);
-
-/* Semaphore */
-sem_t sem;
-
-void *threadpool_thread(void *threadpool){
-	threadpool_t *pool = (threadpool_t *)threadpool;
-	pthread_t id;
-	
-	/* Current semaphore value */
-	int sval;
-
-	id = pthread_self() % 10000;
-
-	while(1){
-		if (!pool->has_assistant){
-			rand_sleep();
-			continue;
-		}
-
-		/* Check maximum waiting */
-		if (pool->count == pool->queue_size){
-			rand_sleep();
-			continue;
-		}
-
-		/* Lock the thread for adding new thread to queue */
-		pthread_mutex_lock(&(pool->lock));
-
-		sem_getvalue(&sem, &sval);
-
-		pool->queue[pool->tail] = id;
-
-		pool->tail += 1;
-		pool->tail = pool->tail % pool->queue_size;
-
-		pool->count += 1;
-
-		/* Wakeup TA is sleeping */
-		if (sval == 0){
-			sem_post(&sem);
-		}
-		
-		pthread_mutex_unlock(&(pool->lock));
-
-		printf("TA helping %lu student and %d students are waiting\n", pool->queue[pool->head], pool->count-1);
-		printf("waiting students : [1] %lu [2] %lu [3] %lu \n", pool->queue[(pool->head+1) % pool->queue_size], 
-			pool->queue[(pool->head+2) % pool->queue_size], pool->queue[(pool->head+3) % pool->queue_size]);
-
-		/* Running task */
-		rand_sleep();
-		
-		/* Lock the thread for removing thread is finished from queue*/
-		pthread_mutex_lock(&(pool->lock));
-
-		printf("%lu student is finish\n\n", pool->queue[pool->head]);
-		
-		pool->queue[pool->head] = 0;
-
-		pool->head += 1;
-		pool->head = pool->head % pool->queue_size;
-
-		pool->count -= 1;	
-
-		pthread_mutex_unlock(&(pool->lock));	
-	}
-}
-
-void *assistant_thread(void *threadpool){
-	threadpool_t *pool = (threadpool_t *)threadpool;
-
-	pool->has_assistant = 1;
-
-	while(1){
-		if(pool->count == 0){
-			sem_wait(&sem);
-		}
-
-		if(pool->count > 0){
-			sleep(1000);
-		}
-	}
-}
+void* stu_programming(void* stu_id);
+void* ta_teaching();
 
 int main(int argc, char **argv){
-	threadpool_t *students;
+
+	//thread(s)
+	pthread_t *students;
 	pthread_t ta;
+	
+	int* student_ids;
 	int student_num;
 	
-	int status;
+	//index
 	int i;
 
-	/* Initialize semaphore */
-	if (sem_init(&sem, 0, 1) == -1){
-		perror("Error");
-		exit(0);
-	}
+	//get number of students from user
+	printf("How many students? ");
+	scanf("%d", &student_num);
 
-	printf("Type number of student : ");
-	scanf("%d", &student_num);	
+	//initialize
+	students = (pthread_t*)malloc(sizeof(pthread_t) * student_num);
+	student_ids = (int*)malloc(sizeof(int) * student_num);
 
-	puts("start");
+	memset(student_ids, 0, student_num);
 
-	students = threadpool_create(student_num, CHAIR_NUM + 1);
-
-	if(pthread_create(&ta, NULL, assistant_thread, (void *)students) != 0){
-		perror("Failed to create thread");
-		exit(0);
-	}
-
-	for (i = 0; i < student_num; i++){
-		pthread_join(students->threads[i], (void *)&status);
-	}
-
-	pthread_join(ta, (void *)&status);
+	sem_init(&sem_stu,0,0);
+	sem_init(&sem_ta,0,1);
 	
-	pthread_mutex_destroy(&(students->lock));
+	//set random
+	srand(time(NULL));
 
+	//sem_init(&mutex,0,1);
+	pthread_mutex_init(&mutex,NULL);
+
+	//create thread
+	pthread_create(&ta,NULL,ta_teaching,NULL);
+
+	//create threads
+	for(i=0; i<student_num; i++)
+	{
+		student_ids[i] = i+1;
+		pthread_create(&students[i], NULL, stu_programming, (void*) &student_ids[i]);
+	} 
+	
+	pthread_join(ta, NULL);
+	
+	for(i=0; i<student_num;i++)
+	{
+		pthread_join(students[i],NULL);
+	}	
+	
 	return 0;
+}
+
+void* stu_programming(void* stu_id)
+{
+	int id = *(int*)stu_id;
+
+	printf("[stu] student %d is programming\n",id);		
+	
+	while(1)
+	{
+		rand_sleep();
+
+		//sem_wait(&mutex);
+		pthread_mutex_lock(&mutex);
+
+		if(count < NUM_SEAT)	
+		{
+			chair[next_seat] = id;
+			count++;
+			
+			printf("	[stu] student %d is waiting\n",id);
+			printf("waiting students : [1] %d [2] %d [3] %d\n",chair[0],chair[1],chair[2]);
+			next_seat = (next_seat+1) % NUM_SEAT;
+			
+			//sem_post(&mutex);
+			pthread_mutex_unlock(&mutex);
+	
+			//wakeup ta
+			sem_post(&sem_stu);
+			sem_wait(&sem_ta);
+		}
+		else //no more chairs
+		{
+			//sem_post(&mutex);
+			pthread_mutex_unlock(&mutex);
+			
+			printf("[stu] no more chairs. student %d is programming\n",id);		
+		}
+	}				
+}
+
+void* ta_teaching()
+{
+	while(1)
+	{
+		sem_wait(&sem_stu);	
+		
+		//sem_wait(&mutex);
+		pthread_mutex_lock(&mutex);
+		
+		printf("		[ta] TA is teaching student %d\n",chair[next_teach]);	
+		chair[next_teach]=0;
+		count--;
+		
+		printf("waiting students : [1] %d [2] %d [3] %d\n",chair[0],chair[1],chair[2]);
+		next_teach = (next_teach + 1) % NUM_SEAT;
+		
+		rand_sleep();
+
+		printf("		[ta] teaching finish.\n");	
+
+		//sem_post(&mutex);
+		pthread_mutex_unlock(&mutex);
+
+		sem_post(&sem_ta);
+	}	
 }
 
 void rand_sleep(void){
 	int time = rand() % SLEEP_MAX + 1;
 	sleep(time);
-}
-
-threadpool_t *threadpool_create(int thread_num, int queue_size){
-	threadpool_t *pool;
-	int i;
-
-	/* Initialize */
-	pool->head = pool->tail = pool->count = 0;
-	pool->queue_size = queue_size;
-
-	/* Allocate threads */
-	pool->threads = (pthread_t *)malloc(sizeof(pthread_t) * thread_num);
-	pool->queue = (long unsigned *)malloc(sizeof(long unsigned) * queue_size);
-
-	if(pthread_mutex_init(&(pool->lock), NULL) != 0) return NULL;
-
-	for(i = 0; i < thread_num; i++){
-		if(pthread_create(&(pool->threads[i]), NULL, threadpool_thread, (void*)pool) != 0){
-			return NULL;
-		}
-		pool->thread_num++;
-	}
-
-	return pool;
 }
